c9c9ff594472b0e0ba31190c29939d8cbaddb771
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2024-10-01 09:45:50 +0300

    add functions to calculate, clear screen and link buttons

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
-const keypad = document.querySelectorAll('button');
-
-keypad.forEach(function (button) {
-    button.addEventListener('click', function () {
-        console.log(button.dataset.value);
-    });
-});
+let currentInput = '';  
+let firstOperand = null; 
+let operator = null; 
+let secondOperand = null; 
+let resultDisplayed = false; 
 
 
+function pressButton(value) {
+    if (resultDisplayed) {
+        currentInput = '';
+        resultDisplayed = false;
+    }
 
-function add (a, b){
+    currentInput += value;
+    document.getElementById('display').innerText = currentInput;
+}
+
+
+function clearScreen() {
+    currentInput = '';
+    firstOperand = null;
+    secondOperand = null;
+    operator = null;
+    document.getElementById('display').innerText = '0';
+}
+
+// Basic math operations
+function add(a, b) {
     return a + b;
 }
 
-function subtract (a, b){
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply (a, b){
+function multiply(a, b) {
     return a * b;
 }
 
-function divide (a, b){
-    return a / b;
+function divide(a, b) {
+    return b === 0 ? 'Error' : a / b;  
 }
 
-function operate (a, operator, b){
-    if (operator === '+'){
-        console.log(add(a, b));
-    }else if (operator === '-'){
-        console.log(subtract(a, b));
-    }else if (operator === '*'){
-        console.log(multiply(a, b));
-    }else if (operator === '/'){
-        console.log(divide(a, b));
+
+function operate(a, operator, b) {
+    a = parseFloat(a);  
+    b = parseFloat(b);  
+
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
     }
 }
 
-operate(15, '/', 10);
+function setOperator(op) {
+    if (firstOperand === null) {
+        firstOperand = currentInput;
+    }
+    operator = op; 
+    currentInput = ''; 
+}
 
+function calculateResult() {
+    if (firstOperand !== null && operator !== null && currentInput !== '') {
+        secondOperand = currentInput;
+        let result = operate(firstOperand, operator, secondOperand);
+
+        document.getElementById('display').innerText = result;
+        currentInput = result.toString();
+        firstOperand = result;
+        operator = null;
+        secondOperand = null;
+        resultDisplayed = true;
+    }
+}
